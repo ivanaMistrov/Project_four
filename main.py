55183ba12279a9837345fa2e2b6a63f6ce847577
KDFX Team <vitalymistrov@gmail.com>
2023-11-20 21:04:47 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# def plus(num_one, num_two):
+#     return (num_one + num_two)
+# def minus(num_one, num_two):
+#     return (num_one - num_two)
+# def dell(num_one, num_two):
+#     return (num_one / num_two)
+# def mult(num_one, num_two):
+#     return (num_one * num_two)
+#
+# num_two
+# while('true'):
+#     num_one = float(input('введите число '))
+#     num_two = float(input('ввыеди второе число : '))
+#     print("Выберите операцию:")
+#     print("1 - Сложение")
+#     print("2 - Вычитание")
+#     print("3 - Умножение")
+#     print("4 - Деление")
+#     operation = int(input("Введите номер операции: "))
+#     if operation == 1:
+#         print(f'сумма :  {plus(num_one, num_two)}')
+#     elif operation == 2:
+#         print(f'сумма :  {minus(num_one, num_two)}')
+#     elif operation == 3:
+#         print(f'сумма :  {mult(num_one, num_two)}')
+#     elif operation == 4:
+#         print(f'сумма : {dell(num_one, num_two)}')
+#     else:
+#         print('не так')
+
